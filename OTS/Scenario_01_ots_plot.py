0a61dbf8011cf6803142bc2fc043b9f3971488bf
ffiedler <felix.mh.fiedler@gmail.com>
2019-02-01 11:44:41 +0100

    Fixed renaming

--- OTS/Scenario_01_ots_plot.py
+++ OTS/Scenario_01_ots_plot.py
@@ -1,105 +1,73 @@
 import numpy as np
-import pandas as pd
-from optimal_traffic_scheduler import optimal_traffic_scheduler
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from casadi import *
 import pdb
+from optimal_traffic_scheduler import optimal_traffic_scheduler
+from ots_visu import ots_plotter
+import distributed_network
+np.random.seed(99)  # Luftballons
 
 
-class input_node:
-    def __init__(self, v_in_traj):
-        """
-        Mimics the behavior of a server node.
-        Contains information about the future package stream of the client.
-        """
-        self.v_in_traj = v_in_traj
-        self.predict = {}
+setup_dict = {}
+setup_dict['n_in'] = 1
+setup_dict['n_out'] = 1
+setup_dict['v_max'] = 20  # mb/s
+setup_dict['s_max'] = 30  # mb
+setup_dict['dt'] = 1  # s
+setup_dict['N_steps'] = 20
+setup_dict['v_delta_penalty'] = 10
 
-    def get_input(self, sim_step, N_horizon):
-        self.predict['v_out'] = self.v_in_traj[sim_step:sim_step+N_horizon]
-
-
-class output_node:
-    def __init__(self, bandwidth_load, memory_load):
-        """
-        Mimics the behavior of a server node.
-        Contains information about the future bandwidth and memory load of the receiver.
-        """
-        self.bandwidth_load = bandwidth_load
-        self.memory_load = memory_load
-        self.predict = {}
-
-    def get_output(self, sim_step, N_horizon):
-        self.predict['bandwidth_load'] = self.bandwidth_load[sim_step:sim_step+N_horizon]
-        self.predict['memory_load'] = self.memory_load[sim_step:sim_step+N_horizon]
+setup_dict.update({'n_out': 2})
+ots_1 = optimal_traffic_scheduler(setup_dict)
+setup_dict.update({'n_out': 1})
+ots_2 = optimal_traffic_scheduler(setup_dict)
+setup_dict.update({'n_in': 2})
+ots_3 = optimal_traffic_scheduler(setup_dict)
+# Input -> ots_1 -> out_2 -> out_3 -> Output
 
 
-class distributed_network:
-    def __init__(self, inputs, outputs, connections, N_horizon):
-        """
-        Define a distributed network:
-        """
-        self.inputs = inputs
-        self.outputs = outputs
-        self.connections = connections
-        self.con_pd = pd.DataFrame(connections)
-        self.sim_step = 0
-        self.N_horizon = N_horizon
+seq_length = 100
+input_mode = 1
 
-    def simulate(self, c_list):
-        # TODO: Option to have multiple inputs.
-        """
-        One item in c_traj_list for each connection.
-        Each connection with dict containing 'source', 'node' and  'connection'
-        """
-        # Check consistency of c matrix (for all elements of sequence)
-        for i, c_i in enumerate(c_list):
-            if not all(np.concatenate([np.sum(c_i_k, axis=0)-1 < 1e-6 for c_i_k in c_i])):
-                raise Exception('Composition Matrix has a row sum that is not equal to 1. Data is lost or created in connection: {}'.format(i))
+# Rows -> Outputs
+# colums -> Inputs
+# Each row sum must be 1 (otherwise data is lost or created.)
+c1 = [np.array([[0.5], [0.5]])]*setup_dict['N_steps']
+c2 = [np.array([[1]])]*setup_dict['N_steps']
+c3 = [np.array([[1, 1]])]*setup_dict['N_steps']
 
-        for input_i in self.inputs:
-            input_i.get_input(self.sim_step, self.N_horizon)
+if input_mode == 1:  # smoothed random values
+    v_in_traj = np.convolve(18*np.random.rand(seq_length), np.ones(seq_length//10)/(seq_length/10), mode='same').reshape(-1, 1)
+    v_in_traj = [v_in_traj[i].reshape(-1, 1) for i in range(v_in_traj.shape[0])]
+if input_mode == 2:  # constant input
+    v_in_traj = [np.array([[8]])]*seq_length
 
-        for output_i in self.outputs:
-            output_i.get_output(self.sim_step, self.N_horizon)
+# Output fo server 3
+bandwidth_traj = [np.array([[0]])]*seq_length
+memory_traj = [np.array([[0]])]*seq_length
 
-        for connection_i, c in zip(self.connections, c_list):
+input_node = distributed_network.input_node(v_in_traj)
+output_node = distributed_network.output_node(bandwidth_traj, memory_traj)
 
-            # Get information from connected nodes:
+connections = [
+    {'source': [input_node], 'node': ots_1, 'target': [ots_2, ots_3]},
+    {'source': [ots_1],      'node': ots_2, 'target': [ots_3]},
+    {'source': [ots_2, ots_1],    'node': ots_3, 'target': [output_node]},
+]
 
-            # Current node:
-            # -----------------------------
-            s0 = connection_i['node'].predict['s'][0]
 
-            # Source node(s):
-            # -----------------------------
-            # For multiple output sources, select respective channel:
+dn = distributed_network.distributed_network([input_node], [output_node], connections, setup_dict['N_steps'])
 
-            v_out_source = []  # All outputs that lead to the current node.
-            for source_i in connection_i['source']:
-                if type(source_i) is input_node:
-                    v_out_source.append(source_i.predict['v_out'])
-                else:
-                    # Use Pandas to find source_node
-                    source_node_ind = self.con_pd.index[self.con_pd['node'] == source_i].tolist()[0]
-                    # Find index of source v_out that is connected to the current node:
-                    source_channel_ind = [connection_i['node'] is source_ots for source_ots in self.con_pd['target'][source_node_ind]]
-                    # Append the respective channel to v_out_source
-                    v_out_source.append([source_i_k[[source_channel_ind]] for source_i_k in source_i.predict['v_out']])
 
-            if len(v_out_source) > 1:
-                v_in = np.hstack(v_out_source)
-            else:
-                v_in = v_out_source[0]
+ots_1_plot = ots_plotter([ots_1, ots_2, ots_3])
 
-            # Target node(s):
-            if type(connection_i['target']) is list:
-                assert len(connection_i['target']) == connection_i['node'].n_out, 'Connection has the wrong number of outputs.'
-                bandwidth_load = np.hstack([source_i.predict['bandwidth_load'] for source_i in connection_i['target']])
-                memory_load = np.hstack([source_i.predict['memory_load'] for source_i in connection_i['target']])
-            else:
-                bandwidth_load = connection_i['target'].predict['bandwidth_load']
-                memory_load = connection_i['target'].predict['memory_load']
 
-            # Simulate Node
-            scheduler_result = connection_i['node'].solve(s0, v_in, c, bandwidth_load, memory_load)
+def update(k):
+    dn.simulate(c_list=[c1, c2, c3])
+    lines = ots_1_plot.plot(k)
+    return lines
 
-        self.sim_step += 1
+
+anim = FuncAnimation(ots_1_plot.fig, update, frames=range(60), repeat=False)
+plt.show()
---
+++ OTS/distributed_network.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+from optimal_traffic_scheduler import optimal_traffic_scheduler
+import pdb
+
+
+class input_node:
+    def __init__(self, v_in_traj):
+        """
+        Mimics the behavior of a server node.
+        Contains information about the future package stream of the client.
+        """
+        self.v_in_traj = v_in_traj
+        self.predict = {}
+
+    def get_input(self, sim_step, N_horizon):
+        self.predict['v_out'] = self.v_in_traj[sim_step:sim_step+N_horizon]
+
+
+class output_node:
+    def __init__(self, bandwidth_load, memory_load):
+        """
+        Mimics the behavior of a server node.
+        Contains information about the future bandwidth and memory load of the receiver.
+        """
+        self.bandwidth_load = bandwidth_load
+        self.memory_load = memory_load
+        self.predict = {}
+
+    def get_output(self, sim_step, N_horizon):
+        self.predict['bandwidth_load'] = self.bandwidth_load[sim_step:sim_step+N_horizon]
+        self.predict['memory_load'] = self.memory_load[sim_step:sim_step+N_horizon]
+
+
+class distributed_network:
+    def __init__(self, inputs, outputs, connections, N_horizon):
+        """
+        Define a distributed network:
+        """
+        self.inputs = inputs
+        self.outputs = outputs
+        self.connections = connections
+        self.con_pd = pd.DataFrame(connections)
+        self.sim_step = 0
+        self.N_horizon = N_horizon
+
+    def simulate(self, c_list):
+        # TODO: Option to have multiple inputs.
+        """
+        One item in c_traj_list for each connection.
+        Each connection with dict containing 'source', 'node' and  'connection'
+        """
+        # Check consistency of c matrix (for all elements of sequence)
+        for i, c_i in enumerate(c_list):
+            if not all(np.concatenate([np.sum(c_i_k, axis=0)-1 < 1e-6 for c_i_k in c_i])):
+                raise Exception('Composition Matrix has a row sum that is not equal to 1. Data is lost or created in connection: {}'.format(i))
+
+        for input_i in self.inputs:
+            input_i.get_input(self.sim_step, self.N_horizon)
+
+        for output_i in self.outputs:
+            output_i.get_output(self.sim_step, self.N_horizon)
+
+        for connection_i, c in zip(self.connections, c_list):
+
+            # Get information from connected nodes:
+
+            # Current node:
+            # -----------------------------
+            s0 = connection_i['node'].predict['s'][0]
+
+            # Source node(s):
+            # -----------------------------
+            # For multiple output sources, select respective channel:
+
+            v_out_source = []  # All outputs that lead to the current node.
+            for source_i in connection_i['source']:
+                if type(source_i) is input_node:
+                    v_out_source.append(source_i.predict['v_out'])
+                else:
+                    # Use Pandas to find source_node
+                    source_node_ind = self.con_pd.index[self.con_pd['node'] == source_i].tolist()[0]
+                    # Find index of source v_out that is connected to the current node:
+                    source_channel_ind = [connection_i['node'] is source_ots for source_ots in self.con_pd['target'][source_node_ind]]
+                    # Append the respective channel to v_out_source
+                    v_out_source.append([source_i_k[[source_channel_ind]] for source_i_k in source_i.predict['v_out']])
+
+            if len(v_out_source) > 1:
+                v_in = np.hstack(v_out_source)
+            else:
+                v_in = v_out_source[0]
+
+            # Target node(s):
+            if type(connection_i['target']) is list:
+                assert len(connection_i['target']) == connection_i['node'].n_out, 'Connection has the wrong number of outputs.'
+                bandwidth_load = np.hstack([source_i.predict['bandwidth_load'] for source_i in connection_i['target']])
+                memory_load = np.hstack([source_i.predict['memory_load'] for source_i in connection_i['target']])
+            else:
+                bandwidth_load = connection_i['target'].predict['bandwidth_load']
+                memory_load = connection_i['target'].predict['memory_load']
+
+            # Simulate Node
+            scheduler_result = connection_i['node'].solve(s0, v_in, c, bandwidth_load, memory_load)
+
+        self.sim_step += 1
