6f5b1def106ae70b3a3b27e847e91b3780358448
ffiedler <felix.mh.fiedler@gmail.com>
2019-02-01 19:22:07 +0100

    New dn and ots approach

---
+++ OTS/distributed_network_02.py
@@ -0,0 +1,192 @@
+import numpy as np
+import pandas as pd
+from optimal_traffic_scheduler_02 import optimal_traffic_scheduler
+import pdb
+
+
+class client_node:
+    def __init__(self, N_horizon, source_fun=None, target_fun=None):
+        """
+        Mimics the behavior of a server node. Can be both source or target of a connection.
+        Inputs:
+        - N_horizon : Prediction horizon.
+        - source_fun: [Optional, when used as target node] function that returns a sequence of package_streams, when evaluated with the current time_step.
+        - target_fun: [Optional, when used as source node] function that returns a sequence of bandwidth_load and memory_load, when evaluated with the current time_step.
+        """
+        self.predict = {}
+        self.source_fun = source_fun
+        self.target_fun = target_fun
+
+    def get_input(self, time_step):
+        if not self.source_fun:
+            raise Exception('source_fun was not defined. Cant get input for client node')
+        else:
+            self.predict['v_out'] = self.source_fun(time_step)
+
+    def get_output(self, time_step):
+        if not self.target_fun:
+            raise Exception('source_fun was not defined. Cant get input for client node')
+        else:
+            self.predict['bandwidth_load'], self.predict['memory_load'] = self.target_fun(time_step)
+
+
+class distributed_network:
+    def __init__(self, circuits):
+        self.connections, self.nodes = self.circ_2_network(circuits)
+        self.analyze_connections()
+
+    def simulate(self):
+        # In every simulation:
+        # a) Determine associated properties for every connection that are determined by the source and target:
+        for i, connection_i in self.connections.iterrows():
+            # Package stream is depending on the source. Create [N_timesteps x n_outputs x 1] array (with np.stack())
+            # and access the element that is stored in 'output_ind' for each connection.
+            connection_i['v'] = np.stack(connection_i['source'].predict['v_out'])[:, [connection_i['output_ind']], :]
+            # Bandwidth and memory load are depending on the target. Create [N_timesteps x 1 x1] array.
+            connection_i['bandwidth_load'] = np.stack(connection_i['target'].predict['bandwidth_load'])
+            connection_i['memory_load'] = np.stack(connection_i['target'].predict['bandwidth_load'])
+
+        # b) Iterate over all nodes, query respective I/O data from connections and simulate node
+        for k, node_k in self.nodes.iterrows():
+            # Simulate only if the node is an optimal_traffic_scheduler.
+            if type(node_k.node) is optimal_traffic_scheduler:
+                # Concatenate package streams for all inputs:
+                v_in = np.concatenate(self.connections.loc[node_k['con_in'], 'v'].values, axis=1)
+                # Concatenate bandwidth and memory load for all outputs:
+                bandwidth_load = np.concatenate(self.connections.loc[node_k['con_out'], 'bandwidth_load'].values, axis=1)
+                memory_load = np.concatenate(self.connections.loc[node_k['con_out'], 'memory_load'].values, axis=1)
+
+                io_mapping = node_k.io_mapping
+                output_partition = node_k.output_partition
+
+                # np.matmul: If either argument is N-D, N > 2, it is treated as a stack of matrices residing in the last two indexes and broadcast accordingly.
+                v_s = output_partition@v_in[:, io_mapping]
+                # Simulate Node
+                # scheduler_result = connection_i['node'].solve(s0, v_in, c, bandwidth_load, memory_load)
+
+    def circ_2_network(self, circuits):
+        """
+        Create nodes and connections DataFrame from circuit dict.
+        Each connection has a new attribute 'circuit' that lists all the circuits that are active.
+        """
+        connections = []
+        nodes = {'node': []}
+        for i, circuit_i in enumerate(circuits):
+            connections.extend([{'source': circuit_i['route'][k], 'target': circuit_i['route'][k+1], 'circuit':i}
+                                for k in range(len(circuit_i['route'])-1)])
+            nodes['node'].extend(circuit_i['route'])
+        con_pd = pd.DataFrame(connections)
+        nodes_pd = pd.DataFrame(nodes).drop_duplicates().reset_index().drop('index', axis=1)
+        # Identify duplicate connections (same source and target) and add which circuit is active.
+        # for i, dupl_con in con_pd[con_pd.duplicated(subset=['source', 'target'])].reset_index().iterrows():
+        #     con_pd[(con_pd.source == dupl_con.source) & (con_pd.target == dupl_con.target)].circuit.values[0].extend(dupl_con['circuit'])
+        # con_pd = con_pd.drop_duplicates(subset=['source', 'target'])
+        return con_pd, nodes_pd
+
+    def analyze_connections(self):
+        """
+        Analyzes the connections and nodes and adds further information
+        """
+        self.connections['output_ind'] = None
+        self.connections['v'] = None
+        self.connections['circ_comp'] = None
+        self.connections['bandwidth_load'] = None
+        self.connections['memory_load'] = None
+
+        self.nodes['con_in'] = None
+        self.nodes['n_in'] = None
+        self.nodes['con_out'] = None
+        self.nodes['n_out'] = None
+        self.nodes['io_mapping'] = None
+        self.nodes['output_partition'] = None
+
+        # Run only once to get information:
+        for k, node_k in self.nodes.iterrows():
+            # Boolean array that indicates in which connections node_k is the source.
+            # This determines how many outputs the current node.
+            node_k['con_out'] = (self.connections['source'] == node_k['node']).values
+            node_k['n_out'] = sum(node_k['con_out'])
+            # The output of each node is a vector with n_out elements. 'output_ind' marks which
+            # of its elements refers to which connection:
+            self.connections.loc[node_k['con_out'], 'output_ind'] = np.arange(node_k['n_out'], dtype='int16').tolist()
+
+            # Boolean array that indicates in which connections node_k is the target. This determines the
+            # number of inputs.
+            node_k['con_in'] = (self.connections['target'] == node_k['node']).values
+            node_k['n_in'] = sum(node_k['con_in'])
+
+            if type(node_k['node']) is optimal_traffic_scheduler:
+                # Query all incoming and outgoing connections and find the respective circuits:
+                source_k = self.connections[node_k['con_out']]
+                target_k = self.connections[node_k['con_in']]
+                input_circ = source_k.circuit.values
+                output_circ = target_k.circuit.values
+                # io_mapping is an index vector that has the same length as the number of circuits in this node.
+                # When indexing the incoming package stream vector with io_mapping the same elements are returned in the order of
+                # the outgoing circuits:
+                node_k['io_mapping'] = np.argsort(input_circ)[np.argsort(output_circ)]
+
+                # Each circuit triggers on connection and therefore incoming package stream for the current node. However, the output buffers
+                # may contain elements of several circuits.
+                output_partition = []
+                for i, source_k_i in source_k.drop_duplicates('target').iterrows():
+                    # Which
+                    output_partition.append((source_k.target == source_k_i.target).values)
+
+                node_k['output_partition'] = np.stack(output_partition)
+                # With io_mapping and output_partion:
+                # v_s = output_partition @ v_in[io_mapping]
+
+                node_k['node'].setup(n_in=node_k['n_in'], n_out=node_k['n_out'])
+
+
+setup_dict = {}
+setup_dict['v_max'] = 20  # mb/s
+setup_dict['s_max'] = 30  # mb
+setup_dict['dt'] = 1  # s
+setup_dict['N_steps'] = 20
+setup_dict['v_delta_penalty'] = 1
+
+ots_1 = optimal_traffic_scheduler(setup_dict)
+ots_2 = optimal_traffic_scheduler(setup_dict)
+ots_3 = optimal_traffic_scheduler(setup_dict)
+ots_4 = optimal_traffic_scheduler(setup_dict)
+# Input -> ots_1 -> out_2 -> out_3 -> Output
+input_node_1 = client_node()
+client_node_2 = client_node()
+input_node_3 = input_node()
+output_node_1 = client_node()
+client_node_2 = client_node()
+output_node_3 = output_node()
+
+circuits = [
+    {'route': [input_node_1, ots_1, ots_2, ots_3, ots_4, output_node_1]},
+    {'route': [input_node_2, ots_1, ots_3, ots_4, output_node_2]},
+    {'route': [input_node_3, ots_1, ots_2, ots_3, ots_4, output_node_3]},
+]
+
+
+dn = distributed_network(circuits)
+
+dn.nodes.loc[1].output_partition
+dn.connections
+
+
+node_k = dn.nodes.loc[1]
+source_k = dn.connections[node_k.node == dn.connections['source']]
+target_k = dn.connections[node_k.node == dn.connections['target']]
+output_circ = source_k.circuit.values  # output = for which connection is the node 'source'
+input_circ = target_k.circuit.values  # input = for which connection is the node 'target'
+# index vector to sort all incoming streams to the appropriate output buffers:
+io_mapping = np.argsort(input_circ)[np.argsort(output_circ)]
+
+# For each unique outgoing connection (find all outgoing connections and drop duplicates):
+output_partition = []
+for i, source_k_i in source_k.drop_duplicates('target').iterrows():
+    # Which
+    output_partition.append((source_k.target == source_k_i.target).values)
+
+output_partition
+
+
+node_k.output_partition
--- OTS/optimal_traffic_scheduler_02.py
+++ OTS/optimal_traffic_scheduler_02.py
@@ -21,7 +21,7 @@ class optimal_traffic_scheduler:
 
     def setup(self, n_in=None, n_out=None):
         """
-        Setup is part of the __init__, if 'n_in' and 'n_out' are already defined.
+        Setup is part of the __init__(), if 'n_in' and 'n_out' are already defined.
         This is not the case, if the ots objects are created as part of a distributed network
         in which the I/O structure is created automatically.
         """
@@ -49,59 +49,55 @@ class optimal_traffic_scheduler:
 
     def initialize_record(self):
         # TODO : Save initial condition (especially for s)
-        self.record = {'time': [], 'v_in': [], 'v_out': [], 'v_in_buffer': [],
-                       's': [], 'c': [], 'bandwidth_load': [], 'memory_load': [],
+        self.record = {'time': [], 'v_in': [], 'v_out': [], 'v_in_buffer': [], 'v_out_buffer': [],
+                       's_circuit': [], 's_buffer': [], 'c': [], 'bandwidth_load': [], 'memory_load': [],
                        'bandwidth_load_target': [], 'memory_load_target': []}
 
     def problem_formulation(self):
-
-        # Composition Matrix
-        c = SX.sym('c', self.n_out, self.n_in)
-
         # Buffer memory
-        s = SX.sym('s', self.n_out, 1)
+        s_buffer = SX.sym('s_buffer', self.n_out, 1)
 
-        # Incoming packet stream
-        v_in = SX.sym('v_in', self.n_in, 1)
+        # Incoming package stream for each buffer:
+        v_in_buffer = SX.sym('v_in_buffer', self.n_out, 1)
+
         # Outgoing packet stream
-        v_out = SX.sym('v_out', self.n_out, 1)
+        v_out_buffer = SX.sym('v_out_buffer', self.n_out, 1)
 
         # bandwidth / memory info outgoing servers
         bandwidth_load = SX.sym('bandwidth_load', self.n_out, 1)
         memory_load = SX.sym('memory_load', self.n_out, 1)
 
         # system dynamics, constraints and objective definition:
-        s_next = s + self.dt*(c@v_in-v_out)
+        s_next = s_buffer + self.dt*(v_in_buffer-v_out_buffer)
         # Note: cons <= 0
         cons = [
             # maximum bandwidth cant be exceeded
-            sum1(v_in)+sum1(v_out) - self.v_max,
-            -s,  # buffer memory cant be <0 (for each output buffer)
-            -v_out,  # outgoing package stream cant be negative
+            sum1(v_in_buffer)+sum1(v_out_buffer) - self.v_max,
+            -s_buffer,  # buffer memory cant be <0 (for each output buffer)
+            -v_out_buffer,  # outgoing package stream cant be negative
         ]
         cons = vertcat(*cons)
 
         # Maximize bandwidth  and maximize buffer:(under consideration of outgoing server load)
         # Note that 0<bandwidth_load<1 and memory_load is normalized by s_max but can exceed 1.
-        obj = sum1(((1-bandwidth_load)/fmax(memory_load, 1))*(-v_out/self.v_max+s/self.s_max))
+        obj = sum1(((1-bandwidth_load)/fmax(memory_load, 1))*(-v_out_buffer/self.v_max+s_buffer/self.s_max))
 
         mpc_problem = {}
         mpc_problem['cons'] = Function(
-            'cons', [v_in, v_out, s, bandwidth_load, memory_load], [cons])
-        mpc_problem['obj'] = Function('obj', [v_in, v_out, s, bandwidth_load, memory_load], [obj])
+            'cons', [v_in_buffer, v_out_buffer, s_buffer, bandwidth_load, memory_load], [cons])
+        mpc_problem['obj'] = Function('obj', [v_in_buffer, v_out_buffer, s_buffer, bandwidth_load, memory_load], [obj])
         mpc_problem['model'] = Function(
-            'model', [v_in, v_out, s, c], [s_next])
+            'model', [v_in_buffer, v_out_buffer, s_buffer], [s_next])
 
         self.mpc_problem = mpc_problem
 
     def create_optim(self):
         # Initialize trajectory lists:
         s_k = []  # [s_1, s_2, ..., s_N]
-        v_in_k = []  # [v_in_0, v_in_1 , ... , v_in_N-1]
-        c_k = []
-        v_out_k = []
-        v_out_prev_k = []
-        v_out_k_delta = []
+        v_buffer_in_k = []  # [v_in_0, v_in_1 , ... , v_in_N-1]
+        v_buffer_out_k = []
+        v_buffer_out_k_prev = []
+        v_buffer_out_k_delta = []
         cons_k = []
         bandwidth_load_k = []
         memory_load_k = []
@@ -112,20 +108,19 @@ class optimal_traffic_scheduler:
         s0 = SX.sym('s0', self.n_out, 1)
 
         for k in range(self.N_steps):
-            # Composition Matrix
-            c_k.append(SX.sym('c', self.n_out, self.n_in))
+
             # Incoming packet stream
-            v_in_k.append(SX.sym('v_in', self.n_in, 1))
+            v_buffer_in_k.append(SX.sym('v_in', self.n_out, 1))
             # Outgoing packet stream
-            v_out_k.append(SX.sym('v_out', self.n_out, 1))
+            v_buffer_out_k.append(SX.sym('v_out', self.n_out, 1))
 
             if k < self.N_steps-1:
-                v_out_prev_k.append(SX.sym('v_out_prev', self.n_out, 1))
-                v_out_k_delta = self.v_delta_penalty*sum1((v_out_k[k]-v_out_prev_k[k])**2)/self.v_max
+                v_buffer_out_k_prev.append(SX.sym('v_out_prev', self.n_out, 1))
+                v_buffer_out_k_delta = self.v_delta_penalty*sum1((v_buffer_out_k[k]-v_buffer_out_k_prev[k])**2)/self.v_max
             else:
-                v_out_k_delta = self.v_delta_penalty*sum1((v_out_k[k]-v_out_k[k-1])**2)/self.v_max
+                v_buffer_out_k_delta = self.v_delta_penalty*sum1((v_buffer_out_k[k]-v_buffer_out_k[k-1])**2)/self.v_max
 
-            obj_k += v_out_k_delta
+            obj_k += v_buffer_out_k_delta
 
             # bandwidth / memory info outgoing servers
             bandwidth_load_k.append(SX.sym('bandwidth_load', self.n_out, 1))
@@ -133,39 +128,34 @@ class optimal_traffic_scheduler:
 
             # For the first step use s0
             if k == 0:
-                s_k.append(self.mpc_problem['model'](v_in_k[k], v_out_k[k], s0, c_k[k]))
+                s_k.append(self.mpc_problem['model'](v_buffer_in_k[k], v_buffer_out_k[k], s0))
             else:  # In suceeding steps use the previous s
-                s_k.append(self.mpc_problem['model'](v_in_k[k], v_out_k[k], s_k[k-1], c_k[k]))
+                s_k.append(self.mpc_problem['model'](v_buffer_in_k[k], v_buffer_out_k[k], s_k[k-1]))
 
             # Add the "stage cost" to the objective
-            obj_k += self.mpc_problem['obj'](v_in_k[k], v_out_k[k],
+            obj_k += self.mpc_problem['obj'](v_buffer_in_k[k], v_buffer_out_k[k],
                                              s_k[k], bandwidth_load_k[k], memory_load_k[k])
             # Constraints for the current step
             cons_k.append(self.mpc_problem['cons'](
-                v_in_k[k], v_out_k[k], s_k[k], bandwidth_load_k[k], memory_load_k[k]))
+                v_buffer_in_k[k], v_buffer_out_k[k], s_k[k], bandwidth_load_k[k], memory_load_k[k]))
 
-        # Parameters for the optimization problem must be a single nx1 vector.
-        # Therefore the composition matrix of each step is reshaped.
-        c_k = [c_i.reshape((-1, 1)) for c_i in c_k]
-
-        optim_dict = {'x': vertcat(*v_out_k),  # Optimization variable
+        optim_dict = {'x': vertcat(*v_buffer_out_k),  # Optimization variable
                       'f': obj_k,  # objective
                       'g': vertcat(*cons_k),  # constraints (Note: cons<=0)
-                      'p': vertcat(s0, *v_in_k,  *c_k, *v_out_prev_k, *bandwidth_load_k, *memory_load_k)}  # parameters
+                      'p': vertcat(s0, *v_buffer_in_k, *v_buffer_out_k_prev, *bandwidth_load_k, *memory_load_k)}  # parameters
 
         # Create casadi optimization object:
         self.optim = nlpsol('optim', 'ipopt', optim_dict)
         # Create function to calculate buffer memory from parameter and optimization variable trajectories
-        self.s_traj = Function('s_traj', v_in_k+v_out_k+[s0]+c_k, s_k)
+        self.s_traj = Function('s_traj', v_s_k+v_buffer_out_k+[s0]+c_k, s_k)
 
-    def solve(self, s0, v_in_traj, c_traj, bandwidth_target_traj, memory_target_traj):
+    def solve(self, s0, v_in_s_traj, bandwidth_target_traj, memory_target_traj):
         """
         Solves the optimal control problem defined in optimal_traffic_scheduler.problem_formulation().
         Inputs:
         - s0            : current memory for each buffer (must be n_out x 1 vector)
         Predicted trajectories (as lists with N_horizon elments):
-        - v_in_traj             : Incoming package stream (n_in x 1 vector)
-        - c_traj                : Composition matrix of incoming package stream (n_out x n_in matrix)
+        - v_in_s_traj           : Incoming package stream for each buffer (n_out x 1 vector)
         - bandwidth_target_traj : Bandwidth load of target server(s) (n_out x 1 vector)
         - memory_target_traj    : Memory load of target server(s) (n_out x 1 vector)
 
@@ -177,24 +167,10 @@ class optimal_traffic_scheduler:
         calculated optimal trajectories.
         """
 
-        # Check if input complies with requirements:
-        if not all([c_traj_k.shape == (self.n_out, self.n_in) for c_traj_k in c_traj]):
-            raise Exception('Composition Matrix is not consistent with the I/O of the node.')
-        if not s0.shape == (self.n_out, 1):
-            raise Exception('Initial buffer storage is not consistent with the I/O of the node.')
-        if not all([v_in_traj_k.shape == (self.n_in, 1) for v_in_traj_k in v_in_traj]):
-            raise Exception('Incoming package stream is not consistent with the I/O of the node.')
-        if not all([bandwidth_target_traj_k.shape == (self.n_out, 1) for bandwidth_target_traj_k in bandwidth_target_traj]):
-            raise Exception('Bandwidth information of connected target server is not consistent with the I/O of the node.')
-        if not all([memory_target_traj_k.shape == (self.n_out, 1) for memory_target_traj_k in memory_target_traj]):
-            raise Exception('Memory information of connected target server is not consistent with the I/O of the node.')
-
-        # Reshape composition matrix for each time step:
-        c_traj_reshape = [c_i.reshape(-1, 1) for c_i in c_traj]
         # Get previous solution:
         v_out_prev_traj = self.predict['v_out']
         # Create concatented parameter vector:
-        param = np.concatenate((s0, *v_in_traj, *c_traj_reshape, *v_out_prev_traj[1:], *bandwidth_target_traj, *memory_target_traj), axis=0)
+        param = np.concatenate((s0, *v_in_s_traj, *v_out_prev_traj[1:], *bandwidth_target_traj, *memory_target_traj), axis=0)
         # Get initial condition:
         x0 = np.concatenate(v_out_prev_traj, axis=0)
         # Solve optimization problem for given conditions:
@@ -205,19 +181,18 @@ class optimal_traffic_scheduler:
         v_out_traj = [x[[k]].T for k in range(self.N_steps)]
 
         # Calculate trajectory of buffer memory usage:
-        s_traj = np.concatenate(self.s_traj(*v_in_traj, *v_out_traj, s0, *c_traj_reshape), axis=1).T
+        s_traj = np.concatenate(self.s_traj(*v_in_s_traj, *v_out_traj, s0, *c_traj_reshape), axis=1).T
         s_traj = [s_traj[[k]].T for k in range(self.N_steps)]
 
         # Calculate trajectory for bandwidth and memory:
         bandwidth_node_traj = [(np.sum(v_out_k, keepdims=True)+np.sum(v_in_k, keepdims=True))/self.v_max for v_out_k,
-                               v_in_k in zip(v_out_traj, v_in_traj)]
+                               v_in_k in zip(v_out_traj, v_in_s_traj)]
 
         memory_node_traj = [np.sum(s_k, keepdims=True)/self.s_max for s_k in s_traj]
 
-        self.predict['v_in'] = v_in_traj
-        self.predict['v_out'] = v_out_traj
-        self.predict['c'] = c_traj
-        self.predict['s'] = s_traj
+        self.predict['v_in_buffer'] = v_in_s_traj
+        self.predict['v_out_buffer'] = v_out_traj
+        self.predict['s_buffer'] = s_traj
         self.predict['bandwidth_load'] = bandwidth_node_traj
         self.predict['memory_load'] = memory_node_traj
         self.predict['bandwidth_load_target'] = np.copy(bandwidth_target_traj)
@@ -227,11 +202,10 @@ class optimal_traffic_scheduler:
 
         if self.record_values:
             self.record['time'].append(np.copy(self.time))
-            self.record['v_in'].append(v_in_traj[0])
-            self.record['v_in_buffer'].append(c_traj[0]@v_in_traj[0])
-            self.record['v_out'].append(v_out_traj[0])
+            self.record['v_in_buffer'].append(v_in_s_traj[0])
+            self.record['v_out_buffer'].append(v_out_traj[0])
             self.record['c'].append(v_out_traj[0])
-            self.record['s'].append(s_traj[0])
+            self.record['s_buffer'].append(s_traj[0])
             self.record['bandwidth_load'].append(bandwidth_node_traj[0])
             self.record['memory_load'].append(memory_node_traj[0])
             self.record['bandwidth_load_target'].append(bandwidth_target_traj[0])
